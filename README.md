4e2fff6b958f4410a03ebf56e2e615754bac6868
SANDHU, PARAMVIR <60945626+SANDHU-PARAMVIR@users.noreply.github.com>
2020-10-30 20:57:14 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+#MidTerm Test - the Favourite Book List App
+
+Student: Param Sandhu
+Student Id: 301118847
---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://amanpreet:amanpreet1234@cluster0.amqwx.mongodb.net/booksfavlist?retryWrites=true&w=majority"
+};
